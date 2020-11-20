e796c0749a3d170f3c85074aeb49a56db77313ed
C. Lente <7017340+clente@users.noreply.github.com>
2020-11-20 19:53:06 -0300

    Exes

---
+++ exercicios/05-vetores.cpp
@@ -0,0 +1,29 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// Crie uma uma função em Rcpp que seja equivalente à função abaixo.
+
+/*** R
+transformar <- function(v) {
+  
+  v2 <- v
+  v2 <- append(v2, 1)
+  v2 <- append(-1, v2)
+  v2[3] <- 0
+  v2 <- sort(v2)
+  
+  list(
+    "length" = length(v),
+    "names"  = names(v),
+    "resto"  = v2
+  )
+}
+*/
+
+// Crie uma uma função em Rcpp que seja equivalente à função abaixo.
+
+/*** R
+selecionar <- function(v, s) {
+  v[s]
+}
+*/
