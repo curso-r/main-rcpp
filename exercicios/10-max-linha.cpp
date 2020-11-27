315d381e24fc83ec6e4e7567518e7587a241b703
Daniel Falbel <dfalbel@gmail.com>
2020-11-27 18:16:42 -0300

    add dica

--- exercicios/10-max-linha.cpp
+++ exercicios/10-max-linha.cpp
@@ -3,7 +3,7 @@ using namespace Rcpp;
 
 // Crie uma função que receba uma matriz e retorne o índice do elemento com
 // maior valor em cada linha.
-
+// dica usar std::max_element: http://www.cplusplus.com/reference/algorithm/max_element/
 
 /*** R
 
