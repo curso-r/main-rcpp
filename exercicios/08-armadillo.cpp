d538477f130a31ed53ced95909409749b3648a9d
Daniel Falbel <dfalbel@gmail.com>
2020-11-27 18:07:39 -0300

    resposta 8

--- exercicios/08-armadillo.cpp
+++ exercicios/08-armadillo.cpp
@@ -1,4 +1,7 @@
-#include <Rcpp.h>
+#include <RcppArmadillo.h>
+#include <RcppArmadilloExtensions/sample.h>
+//[[Rcpp::depends(RcppArmadillo)]]
+
 using namespace Rcpp;
 
 // Crie uma função em RcppArmadillo que seja exatamente equivalente à função
@@ -7,4 +10,14 @@ using namespace Rcpp;
 
 /*** R
 sample(letters[1:10], size = 3, replace = TRUE, prob = rep(1/10, 10))
+*/
+
+// [[Rcpp::export]]
+arma::vec cpp_sample (arma::vec x, int size, bool replace, NumericVector prob)
+{
+  return Rcpp::RcppArmadillo::sample(x, size, replace, prob);
+}
+
+/*** R
+cpp_sample(1:10, 3, TRUE, prob = rep(1/10, 10))
 */
