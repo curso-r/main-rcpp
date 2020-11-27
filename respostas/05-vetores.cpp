0a808d75128a96fe4a565b8ed795c96dbac7a4a5
Daniel Falbel <dfalbel@gmail.com>
2020-11-27 17:29:41 -0300

    resposta 5

--- respostas/05-vetores.cpp
+++ respostas/05-vetores.cpp
@@ -20,6 +20,25 @@ transformar <- function(v) {
 }
 */
 
+// [[Rcpp::export]]
+Rcpp::List cpp_transformar (NumericVector v)
+{
+  NumericVector v2 = v;
+  v2.push_back(1);
+  v2.push_front(-1);
+  v2[2] = 0;
+  v2.sort();
+  return List::create(
+    _["length"] = v.size(),
+    _["names"] = v.names(),
+    _["resto"] = v2
+  );
+}
+
+/*** R
+cpp_transformar(c(1,2,3))
+*/
+
 // Crie uma uma função em Rcpp que seja equivalente à função abaixo.
 
 /*** R
@@ -28,9 +47,36 @@ selecionar <- function(v, s) {
 }
 */
 
+// [[Rcpp::export]]
+double cpp_selecionar (NumericVector v, int s)
+{
+  return v[s-1];
+}
+
+/*** R
+cpp_selecionar(1:10, 5)
+*/
 
 // Crie uma função equivalente à função `rep` do R
 
 /*** R
 rep(c(1,2,3), 3)
+*/
+
+// [[Rcpp::export]]
+NumericVector cpp_rep (NumericVector v, int n)
+{
+  NumericVector out (v.size() * n);
+  for (int i = 0; i < v.size(); i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+     out[i + j*n] = v[i];
+    }
+  }
+  return out;
+}
+
+/*** R
+cpp_rep(c(1,2,3), 3)
 */
