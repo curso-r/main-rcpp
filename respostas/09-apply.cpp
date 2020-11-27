d86445fdaf74a18da88a8ff8a6bc1a5822260958
Daniel Falbel <dfalbel@gmail.com>
2020-11-27 18:12:07 -0300

    resposta 9

--- respostas/09-apply.cpp
+++ respostas/09-apply.cpp
@@ -11,3 +11,19 @@ using namespace Rcpp;
 x <- matrix(runif(1e6), ncol = 10)
 res <- apply(x, 1, mean)
 */
+
+// [[Rcpp::export]]
+NumericVector cpp_apply (NumericMatrix x, Function f)
+{
+  NumericVector out(x.nrow());
+  for (int i = 0; i < x.nrow(); i++)
+  {
+    out[i] = Rcpp::as<double>(f(x(i,_)));
+  }
+  return out;
+}
+
+/*** R
+x <- matrix(runif(100), ncol = 10)
+cpp_apply(x, max)
+*/
